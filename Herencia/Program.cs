9ead58872825c0596c5ae51fe5f6e1e3d978bac4
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-09 12:40:43 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Triangulo triangulo = new Triangulo();
+			triangulo.Color = "amarilla";
+			Console.WriteLine(triangulo.toString());
+
+			triangulo.Altura = 2.5;
+			triangulo.Base = 4;
+
+			double areaTriangulo = triangulo.calcularArea();
+			Console.WriteLine("Area del triangulo: " + areaTriangulo);
+
+			Circulo circulo = new Circulo(5.4);
+			Console.WriteLine("Area del circulo: " + circulo.calcularArea().ToString());
+
+			Figura triangulo2 = new Triangulo();
+			((Triangulo)triangulo).Base = 2;
+			((Triangulo)triangulo).Altura = 3;
+			Console.WriteLine("Area del triangulo 2: " + triangulo2.calcularArea().ToString());
+
+			List<Figura> figuras = new List<Figura>();
+
+			figuras.Add(triangulo);
+			figuras.Add(circulo);
+			figuras.Add(triangulo2);
+
+			foreach(var figura in figuras)
+			{
+				Console.WriteLine("Area: " + figura.calcularArea().ToString());
+			}
+
+			Console.Read();
+
+		}
+	}
+}
